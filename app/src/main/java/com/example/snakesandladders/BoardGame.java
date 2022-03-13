885f88fbef71106e523c8a96ed73fbec2f6a36a5
alon1396 <alon1396@gmail.com>
2022-03-14 00:38:43 +0200

    commit

--- app/src/main/java/com/example/snakesandladders/BoardGame.java
+++ app/src/main/java/com/example/snakesandladders/BoardGame.java
@@ -1,6 +1,9 @@
 package com.example.snakesandladders;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,16 +17,85 @@ public class BoardGame extends AppCompatActivity implements View.OnClickListener
 
     private ImageButton cube;
 
+    private int[][] mat;
+
+    private int i, j, resId;
+
+    private String str;
+
+    private Button restart, back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_board_game);
+
+        manager = new Manager(this);
+
+        cube = (ImageButton) findViewById(R.id.cube);
+        cube.setOnClickListener(this);
+
+        restart = (Button) findViewById(R.id.restart);
+        restart.setOnClickListener(this);
+
+        back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(this);
+
+        mat = manager.getMat();
+
+        pics = new ImageView[9][7];
+        for (i = 0; i < pics.length; i++)
+        {
+            for (j = 0; j < pics[i].length; j++)
+            {
+                str = "imageView" + i + j;
+                resId = getResources().getIdentifier(str, "id", getPackageName());
+                pics[i][j] = (ImageView) findViewById(resId);
+                pics[i][j].setImageResource(mat[i][j]);
+            }
+        }
     }
 
     @Override
     public void onClick(View v)
     {
+        Intent intent = null;
+        if(v.getId() == R.id.back)
+        {
+            intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        if(v.getId() == R.id.restart)
+        {
+            for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j < 7; j++)
+                {
+                    pics[i][j].setImageResource(R.color.transparent);
+                }
+            }
+            pics[8][0].setImageResource(R.drawable.player);
+        }
+        if(v.getId() == R.id.cube)
+        {
+            cube.setImageResource(manager.roll());
+            setPics();
+        }
+    }
 
+    public void setPics()
+    {
+        mat = manager.getMat();
+        for(int i = 0; i < 9; i++)
+        {
+            for(int j = 0; j < 7; j++)
+            {
+                if(mat[i][j] == R.drawable.player)
+                    pics[i][j].setImageResource(mat[i][j]);
+                else
+                    pics[i][j].setImageResource(R.color.transparent);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/snakesandladders/Manager.java
+++ app/src/main/java/com/example/snakesandladders/Manager.java
@@ -1,21 +1,26 @@
 package com.example.snakesandladders;
 
+import android.util.Log;
+
 import java.util.Random;
 
-public class Manager
-{
+public class Manager {
     private BoardGame boardGame;
 
-    public static final int maxX = 7;
-    public static final int maxY = 9;
-
-    private int[][] mat;
-    private Special[] special;
     private int money;
 
-    private Player player;
+    public static final int maxY = 9;
+    public static final int maxX = 7;
 
-    public static final int[] cubes = new int[] {
+    private boolean isLeft; // T - לך ימינה F - לך שמאלה
+    private int[][] mat;
+    private Special[] special;
+
+    private int playerY;
+    private int playerX;
+
+
+    public static final int[] cubes = new int[]{
             0,
             R.drawable.cube1,
             R.drawable.cube2,
@@ -25,20 +30,21 @@ public class Manager
             R.drawable.cube6
     };
 
-    public Manager(BoardGame boardGame)
-    {
+    public Manager(BoardGame boardGame) {
         this.boardGame = boardGame;
-        this.player = new Player(0, 0);
-        this.mat = new int[maxX][maxY];
-        for (int i = 0; i < this.mat.length; i++)
-        {
-            for (int j = 0; j < this.mat[0].length; j++)
-            {
-                this.mat[i][j] = R.drawable.null_square;
+        this.playerY = 8;
+        this.playerX = 6;
+        mat = new int[maxY][maxX];
+        this.isLeft = true;
+        for (int i = 0; i < maxY; i++) {
+            for (int j = 0; j < maxX; j++) {
+                mat[i][j] = R.drawable.null_square;
             }
         }
-        this.mat[0][0] = R.drawable.player;
-        System.out.println(this.mat[0][5]);
+        mat[maxY - 1][maxX - 1] = R.drawable.player;
+        this.money = 0;
+
+
         this.special = new Special[]{
                 new Special(2, 0, 2, 2, "l"),
                 new Special(5, 2, 4, 4, "l"),
@@ -51,53 +57,52 @@ public class Manager
                 new Special(2, 6, 1, 8, "s"),
         };
     }
+//    public void isLeft(){
+//        if(playerX > 6)
+//            this.isLeft = false;
+//        if(playerX < 0)
+//            this.isLeft = true;
+//    }
 
     public void movePlayer(int steps) {
-        this.money += steps * 30000;
-        int x = steps + player.getX();
-        int y = this.player.getY();
-        int prevX = x;
-        int prevY = y;
-        if (x >= maxX) {
-            x = x - maxX;
-            if (y < maxY) {
-                y++;
-            } else {
-                x = maxX - (maxX - steps) - 1;
-            }
-        }
-        this.player.setX(x);
-        this.player.setY(y);
-
-        moveSpecial();
-
-        this.mat[this.player.getX()][this.player.getY()] = R.drawable.player;
-        this.mat[prevX][prevY] = R.drawable.null_square;
+        int lastY = playerY;
+        int lastX = playerX;
+        if (this.isLeft)
+            movePlayerLeft(steps);
+        else
+            movePlayerRight(steps);
+        mat[lastY][lastX] = R.drawable.null_square;
+        mat[playerY][playerX] = R.drawable.player;
     }
 
-    public void moveSpecial() {
-        int x = this.player.getX();
-        int y = this.player.getY();
-        for (int i = 0; i < this.special.length; i++) {
-            Special special = this.special[i];
-            if (special.getType() == "s") {
-                if (special.getEndX() == x && special.getEndY() == y) {
-                    this.money -= 30000 * ((special.getEndX() + 1) * (special.getEndY() + 1) - (special.getStartX() + 1) * (special.getStartY() + 1));;
-                    if (this.money < 0) this.money = 0;
-                    this.player.setX(special.getStartX());
-                    this.player.setY(special.getStartY());
-                    return;
-                }
+    public void movePlayerLeft(int steps) {
+        int newY = playerY;
+        int newX = playerX - steps;
 
-            } else if (special.getType() == "l") {
-                if (special.getStartX() == x && special.getStartY() == y) {
-                    this.money += 30000 * ((special.getEndX() + 1) * (special.getEndY() + 1) - (special.getStartX() + 1) * (special.getStartY() + 1));
-                    this.player.setX(special.getEndX());
-                    this.player.setY(special.getStartY());
-                    return;
-                }
+        if (newX < 0) { // אם שלילי אז צריך לטפל ולהוריד שורה
+            if (newY != 0) {
+                newX = steps - 1 - playerX;
+                newY--;
+                isLeft = !isLeft;
+            }
+            else {
+                newX = maxX - steps;
             }
         }
+        playerY = newY;
+        playerX = newX;
+    }
+    public void movePlayerRight (int steps) {
+        int newY = playerY;
+        int newX = playerX + steps;
+
+        if(newX > 6){
+            newX = maxX - (newX - maxX) - 1;
+            newY--;
+            isLeft = !isLeft;
+        }
+        playerY = newY;
+        playerX = newX;
     }
 
     public int roll() {
@@ -107,11 +112,11 @@ public class Manager
         return cubes[num];
     }
 
-    public int[][] getMat() {
-        return mat;
-    }
-
     public int getMoney() {
         return money;
+    }
+
+    public int[][] getMat() {
+        return mat;
     }
 }
